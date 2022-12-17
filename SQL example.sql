98211614b96fc10ad9c9fbee6d9797676f0fbd61
sumitxsaraswat@gmail.com <sumitxsaraswat@gmail.com>
2022-12-17 20:18:36 +0000

    First Draft

---
+++ SQL example.sql
@@ -0,0 +1,19 @@
+-- Databricks notebook source
+-- MAGIC %md
+-- MAGIC ### Example of create a delta table
+
+-- COMMAND ----------
+
+Create table sample (columna string)
+
+-- COMMAND ----------
+
+insert into sample values ("john")
+
+-- COMMAND ----------
+
+select * from sample
+
+-- COMMAND ----------
+
+
